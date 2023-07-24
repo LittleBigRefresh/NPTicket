375da4f2509c0121b7c873d93598fb5217bf5869
jvyden <jvyden@jvyden.xyz>
2023-07-23 23:28:26 -0400

    Document PsnSigningKey

--- NPTicket/Verification/Keys/PsnSigningKey.cs
+++ NPTicket/Verification/Keys/PsnSigningKey.cs
@@ -1,5 +1,9 @@
 namespace NPTicket.Verification.Keys;
 
+/// <summary>
+/// A public signing key used when PS3/PSVita clients connect via official PSN.
+/// Since each game series has a different set of curves, you must provide them yourself in a new class.
+/// </summary>
 public abstract class PsnSigningKey : ITicketSigningKey
 {
     public string HashAlgorithm => "SHA-1";
